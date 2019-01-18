fcbd0a8ac5e7d0013e2a7bd91ded7c4571fbe7b3
Esger Jellema <esgerj@gmail.com>
2019-01-18 02:14:13 +0100

    Tiles with values

--- src/resources/elements/board/board.js
+++ src/resources/elements/board/board.js
@@ -1,3 +1,19 @@
+
 export class BoardCustomElement {
-    constructor() { }
+
+    constructor() {
+        this.boardSize = 7;
+        this.row = [];
+        this.board = [];
+        for (let x = 0; x < this.boardSize; x++) {
+            this.row.push(1);
+        }
+        for (let y = 0; y < this.boardSize; y++) {
+            this.board.push(this.row);
+        }
+    }
+
+    attached() {
+        console.table(this.board);
+    }
 }

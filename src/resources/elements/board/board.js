879d3a845366cc6489216ec52227e7e074771260
Esger Jellema <esgerj@gmail.com>
2019-01-23 22:14:47 +0100

    Working! No signaling (array of objects), no tile custom element
    Boardsize can be set in combination with tiles per row

--- src/resources/elements/board/board.js
+++ src/resources/elements/board/board.js
@@ -11,26 +11,33 @@ export class BoardCustomElement {
         this.dragService = dragService;
         this._bindingSignaler = bindingSignaler;
         this._eventAggregator = eventAggregator;
-        this._boardSize = 7;
+        this._boardSize = 5; // / @boardSize
+        this._tileSize = 8;
         this._center = Math.floor(this._boardSize / 2);
         this._highestValue = 1;
         this._score = 0;
         this.board = [];
         this.showBoard = true;
-        this.distance = 9.666;
-        this.offset = 2;
+        // this.distance = 9.666;
+        this.offset = this._boardSize * 2 / (this._boardSize + 1);
+        this.distance = this._tileSize + this.offset;
     }
 
-    _tile() {
-        return { value: 1 };
+    _tile(x, y) {
+        return {
+            value: 1,
+            id: 'tile_' + y + '-' + x
+        };
     }
 
     attached() {
-        this._tileWidth = $('tile').width() / 9.666 * 11.666;
+        setTimeout(() => {
+            this._tileWidth = $('.tile').width() / this.distance * (this.distance + 2);
+        });
         for (let y = 0; y < this._boardSize; y++) {
             let row = [];
             for (let x = 0; x < this._boardSize; x++) {
-                row.push(this._tile());
+                row.push(this._tile(x, y));
             }
             this.board.push(row);
         }
@@ -63,11 +70,14 @@ export class BoardCustomElement {
                     let targetValue = this.board[target[0]][target[1]].value;
                     this._releaseTile = true;
                     if (this._draggedValue == targetValue) {
+                        // animate dragged tile to target
                         this._moveTile(signs[1] * this._tileWidth, signs[0] * this._tileWidth);
                         this._doubleTile(target);
+                        let tilesBehind = this._findTilesBehind(signs);
+                        // animate the tiles on the board
+                        this._moveTiles(tilesBehind, signs);
                         setTimeout(() => {
-                            // this._shiftTiles(signs);
-                            this._shiftBoardTiles(signs);
+                            this._shiftTilesBehind(tilesBehind);
                         }, 500);
 
                     } else {
@@ -89,56 +99,31 @@ export class BoardCustomElement {
         });
     }
 
-    _shiftTiles(signs) {
-        this._$tile.remove();
-        let offset = signs.slice();
-        let dataId = 'tile_' + (this._dragTileIndex[0] - offset[0]) + '-' + (this._dragTileIndex[1] - offset[1]);
-        let $tiles = [];
-        let $tile = $('[data-id=' + dataId + ']');
-        let toIndex = this._dragTileIndex.slice();
-        let fromIndex = toIndex.slice();
-        fromIndex[0] = toIndex[0] - signs[0];
-        fromIndex[1] = toIndex[1] - signs[1];
-        while ($tile.length) {
-            this.board[toIndex[0]][toIndex[1]].value = this.board[fromIndex[0]][fromIndex[1]].value;
-            $tiles.push($tile);
-            fromIndex[0] = toIndex[0] - signs[0];
-            fromIndex[1] = toIndex[1] - signs[1];
-            offset[0] += signs[0];
-            offset[1] += signs[1];
-            dataId = 'tile_' + (this._dragTileIndex[0] - offset[0]) + '-' + (this._dragTileIndex[1] - offset[1]);
-            $tile = $('[data-id=' + dataId + ']');
-        }
-        // this.board[toIndex[0]][toIndex[1]] = Math.ceil(Math.random() * this._highestValue);
-        let dx = signs[1] * this._tileWidth;
-        let dy = signs[0] * this._tileWidth;
-        let dt = 50;
-        $tiles.forEach($t => {
-            $t.addClass('follow');
+    _moveTile(x, y) {
+        this._$tile.addClass('attracted');
+        setTimeout(() => {
+            this._$tile.css({
+                transform: 'translate(' + x + 'px, ' + y + 'px)'
+            });
             setTimeout(() => {
-                // $t.animate({ top: dx, left: dy }, 100, 'easeOutBounce');
-                $t.css({
+                this._$tile.removeClass('attracted correct');
+            }, 200);
+        });
+    }
+
+    _moveTiles(tiles, signs) {
+        tiles.forEach(tile => {
+            let $tile = $('#' + tile.id);
+            $tile.addClass('follow');
+            let dx = signs[1] * this._tileWidth;
+            let dy = signs[0] * this._tileWidth;
+            let dt = 50;
+            setTimeout(() => {
+                $tile.css({
                     transform: 'translate(' + dx + 'px, ' + dy + 'px)'
                 });
             }, dt);
-            dt += 50;
         });
-        this._bindingSignaler.signal('update-id');
-        this._bindingSignaler.signal('update-value');
-        console.table(this.board);
-    }
-
-    getTile(pos) {
-        return this.board[pos[1]][pos[0]].value;
-    }
-
-    setTile(pos, value) {
-        this.board[pos[1]][pos[0]].value = value;
-    }
-
-    _shiftBoardTiles(directions) {
-        let tilesBehind = this._findTilesBehind(directions);
-        this._shiftTilesBehind(tilesBehind);
     }
 
     _withinBoundaries(target) {
@@ -163,13 +148,21 @@ export class BoardCustomElement {
 
     // shift the tiles behind 1 place in same direction as target and fill outermost tile with random power of 2 smaller than highestValue.
     _shiftTilesBehind(tiles, directions) {
+        $('.tile').css({
+            transform: 'translate(0, 0)'
+        });
         let last = tiles.length - 1;
         for (let i = 0; i < last; i++) {
             this.board[tiles[i][0]][tiles[i][1]].value = this.board[tiles[i + 1][0]][tiles[i + 1][1]].value;
         }
-        this.board[tiles[last][0]][tiles[last][1]].value = Math.ceil(Math.random() * this._highestValue);
+        let power = 0;
+        let high = this._highestValue;
+        while (high > 1) {
+            high /= 2;
+            power++;
+        }
+        this.board[tiles[last][0]][tiles[last][1]].value = Math.pow(2, Math.ceil(Math.random() * power));
         this._bindingSignaler.signal('update-value');
-        console.table(this.board);
     }
 
     _doubleTile(pos) {
@@ -189,12 +182,6 @@ export class BoardCustomElement {
         setTimeout(() => {
             this._$tile.removeClass('dragging attracted retracted incorrect');
         }, 500);
-    }
-
-    _moveTile(x, y) {
-        this._$tile.css({
-            transform: 'translate(' + x + 'px, ' + y + 'px)'
-        });
     }
 
     _underTreshold(constrainedDistance) {

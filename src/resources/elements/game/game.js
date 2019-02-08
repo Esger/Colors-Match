9bc24cfde830098406a121fc524f77deebe6a54d
Esger Jellema <esgerj@gmail.com>
2019-02-08 01:36:28 +0100

    Highscore qr hover

--- src/resources/elements/game/game.js
+++ src/resources/elements/game/game.js
@@ -1,18 +1,25 @@
 import { inject } from 'aurelia-framework';
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { DragService } from 'resources/services/drag-service';
+import { ScoreService } from 'resources/services/score-service';
 
-@inject(DragService, EventAggregator)
+@inject(DragService, ScoreService, EventAggregator)
 export class GameCustomElement {
 
-    constructor(dragService, eventAggregator) {
+    constructor(dragService, scoreService, eventAggregator) {
         this.dragService = dragService;
         this._eventAggregator = eventAggregator;
+        this._scoreService = scoreService;
         this._resetScore();
     }
 
     attached() {
+        this.highScore = this._scoreService.getScore();
         this.highSubscriber = this._eventAggregator.subscribe('high', value => {
+            if (value > this.highScore) {
+                this.highScore = value;
+                this._scoreService.saveScore(value);
+            }
             this.title = value + '+' + value;
         });
         this.resetScoreSubscriber = this._eventAggregator.subscribe('reset-score', value => {
